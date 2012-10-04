db4eba08c050a5af192df3c4cedb84d61cb6b945
U-GOOGLE\johnmccutchan <johnmccutchan@JOHNMCCUTCHA0-W.ad.corp.google.com>
2012-10-04 08:51:05 -0700

    Add empty NaClAMBullet

---
+++ NaClAMBullet/NaClAMBullet.cpp
@@ -0,0 +1,25 @@
+#include "NaClAMBase/NaClAMBase.h"
+
+/**
+ * This function is called at module initialization time.
+ * moduleInterfaces and moduleInstance are already initialized.
+ */
+void NaClAMModuleInit() {
+
+}
+
+/**
+ * This function is called at 60hz.
+ * @param microseconds A monotonically increasing clock
+ */
+void NaClAMModuleHeartBeat(uint64_t microseconds) {
+
+}
+
+/**
+ * This function is called for each message received from JS
+ * @param message A complete message sent from JS
+ */
+void NaClAMModuleHandleMessage(const NaClAMMessage& message) {
+
+}
